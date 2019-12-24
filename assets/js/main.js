86dcaea185dd4b5238ac14afb3593cbb1e5dde62
James Simpson <james@goldfirestudios.com>
2019-12-24 16:44:16 -0600

    Update howler.js to v2.1.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ var modalTitle = document.querySelector('.modal-title');
     counter.innerText = (count ? count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '14,000+') + ' stars';
   };
   xhr.onerror = function() {
-    counter.innerText = '14,000+ stars';
+    counter.innerText = '15,000+ stars';
   };
   xhr.send();
 })();
